17ebc3d758c283c4f151c600527fafae1efe2a4e
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-09-27 15:11:19 +0400

    end recaptcha v3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,10 +10,21 @@ export class AppController {
     return this.appService.getHello()
   }
 
+  // v3
   @Post('password_recovery')
   async passwordRecovery(@Body() body: { email: string; recaptchaToken: string }) {
     if (await recaptchaAdapter.isValid(body.recaptchaToken)) {
-      return `you are human`
+      return JSON.stringify(`you are human!!!`)
+    } else {
+      throw new BadRequestException('recaptcha error')
+    }
+  }
+
+  // v2
+  @Post('password_recovery_v2')
+  async passwordRecoveryV2(@Body() body: { email: string; recaptchaToken: string }) {
+    if (await recaptchaAdapter.isValidV2(body.recaptchaToken)) {
+      return JSON.stringify(`you are human!!!`)
     } else {
       throw new BadRequestException('recaptcha error')
     }
@@ -32,6 +43,20 @@ const recaptchaAdapter = {
     }).then((res) => res.json())
 
     console.log(result)
+
+    return result.success
+  },
+  async isValidV2(value: string): Promise<boolean> {
+    const result = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+      method: 'POST',
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        secret: '6LcrDE8qAAAAAIdoQRMq14NPG1hOqh1iIxvw3ggQ',
+        response: value,
+      }),
+    }).then((res) => res.json())
+
+    console.log(result, value)
 
     return result.success
   },
